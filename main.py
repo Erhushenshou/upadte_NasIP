15eabf792b20cb7073ccc3c83b3857b96332e6e7
Erhushenshou <38848249+Erhushenshou@users.noreply.github.com>
2022-03-19 11:42:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ subdomainInfo = {((i['name'], i['line']) if i['type'] == 'A' else ('invalid', 'i
 
 
 
-updated_ip = getMyIp()
+updated_ip = 219.137.65.160
 http_data =[{
                 **loginInfo,
                 **{
